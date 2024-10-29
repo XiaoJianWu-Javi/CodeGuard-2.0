87e78d452b7cd8447f8448575077570f1444b7ca
Diego Recio <diego.recio@metrica.es>
2024-10-29 08:32:11 +0100

    Initial Commit

---
+++ src/main/java/es/tfg/codeguard/CodeGuardApplication.java
@@ -0,0 +1,13 @@
+package es.tfg.codeguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/tfg/codeguard/CodeGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package es.tfg.codeguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
