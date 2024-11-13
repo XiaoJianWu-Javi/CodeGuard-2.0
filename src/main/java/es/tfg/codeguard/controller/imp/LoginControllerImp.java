805cd5a01700f06042eb1d8ffbb25cff7436383b
Diego Recio <diegorecio22@outlook.es>
2024-11-13 11:24:30 +0100

    Commit rapid bugfix

--- src/main/java/es/tfg/codeguard/controller/imp/LoginControllerImp.java
+++ src/main/java/es/tfg/codeguard/controller/imp/LoginControllerImp.java
@@ -6,9 +6,57 @@ import es.tfg.codeguard.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class LoginControllerImp implements LoginController {
+    //TODO: Use interface
+    //TODO: Use interface
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    
+    @PostMapping(name = "/login", consumes = "application/json")
+    public ResponseEntity login(@RequestBody AuthRequest user) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+            );
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch(AuthenticationException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    class AuthRequest {
+        private String username;
+        private String password;
+
+        
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        
+        public void setPassword(String password) {
+            this.password = password;
+        }
+        
+    }
 
     @Autowired
     private UserService userService;
