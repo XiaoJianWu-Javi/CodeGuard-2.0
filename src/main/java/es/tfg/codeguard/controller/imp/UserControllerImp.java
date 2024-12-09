ea71bf0debfb52a27fa24a96ce8e5c98871c7809
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-12-09 12:22:01 +0100

    Update UserControllerImp

--- src/main/java/es/tfg/codeguard/controller/imp/UserControllerImp.java
+++ src/main/java/es/tfg/codeguard/controller/imp/UserControllerImp.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import es.tfg.codeguard.controller.UserController;
 import es.tfg.codeguard.model.dto.UserDTO;
+import es.tfg.codeguard.model.dto.UserRestoreDTO;
 import es.tfg.codeguard.service.UserService;
 
 @RestController
