b61f52c37717d050c024e6581a49ed837c887891
XiaoJianWu-Javi <wuhuairu12@gmail.com>
2024-12-10 10:33:49 +0100

    Update ExcerciseReportRepository
    Añadimos un find personalizado en la que se devuelve un optional de excerciseReport

--- src/main/java/es/tfg/codeguard/model/repository/exercisereport/ExerciseReportRepository.java
+++ src/main/java/es/tfg/codeguard/model/repository/exercisereport/ExerciseReportRepository.java
@@ -1,6 +1,14 @@
 package es.tfg.codeguard.model.repository.exercisereport;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import es.tfg.codeguard.model.entity.exercisereport.ExerciseReport;
+import java.util.List;
 
-public interface ExerciseReportRepository extends JpaRepository<ExerciseReport, Long> {}
+
+public interface ExerciseReportRepository extends JpaRepository<ExerciseReport, Long> {
+	
+	Optional<ExerciseReport> findByUserNameAndDescription(String userName, String description);
+}
+
